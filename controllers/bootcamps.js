d78382674ad44c2da82d799c8e6022375eefebff
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2021-01-16 21:03:59 +0300

    reset password

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -29,19 +29,45 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
+
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req.body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamps
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If the user is not an admin, they can only add one bootcamp
+
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({ success: true, data: bootcamp });
 });
+
 // @desc      Update bootcamp
 // @route     PUT /api/v1/bootcamps/:id
 // @access    Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+  let bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) return res.status(400).json({ success: false });
+
+  // Make sure user is bootcamp owner
+
+  if (isOwner(bootcamp.user, req.user.id, req.user.role, 'update', next));
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
-  if (!bootcamp) return res.status(400).json({ success: false });
 
   res.status(200).json({ success: true, data: bootcamp });
 });
@@ -56,6 +82,16 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
+
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete this bootcamp`,
+        401
+      )
+    );
+  }
+  // if (isOwner(bootcamp.user, req.user.id, req.user.role, 'delete', next));
 
   bootcamp.remove();
 
@@ -100,6 +136,15 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
 
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+
   if (!req.files) return next(new ErrorResponse('Please upload a file', 400));
 
   const file = req.files.file;
@@ -136,3 +181,14 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
     });
   });
 });
+
+const isOwner = (bootcampUserId, reqUserId, userRole, action, next) => {
+  if (bootcampUserId.toString() !== reqUserId && userRole !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${reqUserId} is not authorized to ${action} this bootcamp`,
+        401
+      )
+    );
+  }
+};
---
+++ utils/sendEmail.js
@@ -0,0 +1,38 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  //   let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_EMAIL, // generated ethereal user
+      pass: process.env.SMTP_PASSWORD, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  const info = await transporter.sendMail(message);
+
+  //   let info = await transporter.sendMail({
+  //     from: '"Fred Foo 👻" <foo@example.com>', // sender address
+  //     to: "bar@example.com, baz@example.com", // list of receivers
+  //     subject: "Hello ✔", // Subject line
+  //     text: "Hello world?", // plain text body
+  //     html: "<b>Hello world?</b>", // html body
+  //   });
+
+  console.log('Message sent: %s', info.messageId);
+};
+
+module.exports = sendEmail;
