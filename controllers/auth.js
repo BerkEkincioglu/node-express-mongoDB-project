f546cf5b68e95093377d961e5af5827e08c0de62
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2021-01-17 18:13:01 +0300

    admin controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,6 @@ const crypto = require('crypto');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
-const { unwatchFile } = require('fs');
 
 // @desc      Register user
 // @route     POST /api/v1/auth/register
@@ -59,6 +58,38 @@ exports.login = asyncHandler(async (req, res, next) => {
 
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
+
+  res.status(200).json({ success: true, data: user });
+});
+
+// @desc     Update password
+// @route     PUT /api/v1/auth/updatepassword
+// @access    Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401));
+  }
+  user.password = req.body.newPassword;
+  await user.save();
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc     Update user details
+// @route     PUT /api/v1/auth/updatedetails
+// @access    Private
+
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({ success: true, data: user });
 });
